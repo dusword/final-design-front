153485dd52b50f9461d3713037d6ac54e1bd9672
lixiangjie <773983534@qq.com>
2021-04-25 16:02:46 +0800

    单一预测如果阴性则显示阴性

--- src/views/Testtest.vue
+++ src/views/Testtest.vue
@@ -1,29 +1,41 @@
 <template>
   <el-container>
-  <el-upload
-      class="upload-demo"
-      drag
-      action="http://localhost:8082/predict/onePic"
-      :on-success="handleSuccess"
-      multiple>
-    <i class="el-icon-upload"></i>
-    <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-    <div class="el-upload__tip" slot="tip">只能上传1张jpg文件</div>
+    <el-upload
+        class="upload-demo"
+        drag
+        action="http://localhost:8082/predict/onePic"
+        :on-success="handleSuccess"
+        multiple>
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      <div class="el-upload__tip" slot="tip">只能上传1张jpg文件</div>
       <!--      <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>-->
-  </el-upload>
+    </el-upload>
 
-  <el-dialog
-      title="分析结果"
-      :visible.sync="dialogVisible"
-      width="60%"
-      :before-close="handleClose">
-    <span></span>
-    <img :src="base64" alt="图片未上传" style="position: center"/>
-    <span slot="footer" class="dialog-footer">
-    <el-button @click="dialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    <el-dialog
+        title="分析结果"
+        :visible.sync="dialog1Visible"
+        width="60%"
+        :before-close="handleClose">
+      <span></span>
+      <img :src="base64" alt="图片未上传" style="position: center"/>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialog1Visible = false">取 消</el-button>
+    <el-button type="primary" @click="dialog1Visible = false">确 定</el-button>
   </span>
-  </el-dialog>
+    </el-dialog>
+    <el-dialog
+        title="分析结果"
+        :visible.sync="dialog2Visible"
+        width="60%"
+        :before-close="handleClose">
+      <span></span>
+      二分检测结果为：阴性
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialog2Visible = false">取 消</el-button>
+    <el-button type="primary" @click="dialog2Visible = false">确 定</el-button>
+  </span>
+    </el-dialog>
 
   </el-container>
 
@@ -34,23 +46,29 @@ export default {
   name: "Testtest",
   data() {
     return {
-      responseList:[],
+      responseList: [],
       fileList: [],
-      dialogVisible: false,
-      response:null,
-      base64:null,
+      dialog1Visible: false,
+      dialog2Visible: false,
+      response: null,
+      base64: null,
     };
   },
   methods: {
     handleSuccess(response) {
       const _this = this;
-      this.responseList=response.data;
+      this.responseList = response.data;
       console.log(response)
       this.$message.success('图片分析成功')
-      this.$data.dialogVisible = true
-      this.$data.response=response
-      console.log(response.base64_result)
-      this.$data.base64="data:image/jpeg;base64,"+response.base64_result
+      this.$data.response = response
+      // console.log(response.base64_result)
+      console.log(response.final_result)
+      if (this.response.final_result === 1) {
+        this.$data.dialog2Visible = true
+      } else if (this.response.final_result === 0) {
+        this.$data.base64 = "data:image/jpeg;base64," + response.base64_result
+        this.$data.dialog1Visible = true
+      }
     },
     submitUpload() {
       this.$refs.upload.submit();
